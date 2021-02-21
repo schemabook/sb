1e485957c5bb7ba6b239b0db9e08d2e3bad2eb7b
Mike Krisher <mike@mikekrisher.com>
2021-02-21 12:26:48 -0500

    Adds tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,15 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
       }
     }
-  }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ]
 }
