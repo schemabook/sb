af862a2823481187b6ddb8b19d8322d195bfd6ab
Mike Krisher <mike@mikekrisher.com>
2021-03-02 20:05:13 -0500

    Specifies all of the marketing routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,23 @@ Rails.application.routes.draw do
 
   ### Marketing pages
   get 'features', to: 'public#features'
+    get 'features/producers', to: 'public#producers'
+    get 'features/consumers', to: 'public#consumers'
+    get 'features/api_admins', to: 'public#api_admins'
+    get 'features/database_admins', to: 'public#database_admins'
   get 'pricing', to: 'public#pricing'
   get 'company', to: 'public#company'
+    get 'about', to: 'public#company'
+
+  get 'docs', to: 'public#docs'
+  get 'guides', to: 'public#guides'
+  get 'status' => redirect("http://twitter.com/schemabook_status")
+
+  get 'blog' => redirect("http://medium.com/schemabook")
+
+  get 'privacy', to: 'public#privacy'
+  get 'terms', to: 'public#terms'
+
+  ### Application routes
+
 end
