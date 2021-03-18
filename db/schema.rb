6593d52d20762f6e24d8d4913b8db10ebabd8a20
Mike Krisher <mike@mikekrisher.com>
2021-03-18 09:36:41 -0400

    Scaffolds user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_144438) do
+ActiveRecord::Schema.define(version: 2021_03_18_133442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "schemas", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
