0451c3061f264945f2f993c36d54cc71f897db0e
Mike Krisher <mike@mikekrisher.com>
2021-05-19 17:38:44 -0400

    Adds attributes to Business, permits updating them through Business#edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_223752) do
+ActiveRecord::Schema.define(version: 2021_05_19_212416) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +20,11 @@ ActiveRecord::Schema.define(version: 2021_04_21_223752) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
     t.integer "created_by"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "postal"
+    t.string "country"
     t.index ["name"], name: "index_businesses_on_name", unique: true
   end
 
@@ -40,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_04_21_223752) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
