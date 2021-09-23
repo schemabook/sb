770bca987d83677a3ffa97b8d31bbbf3960d2be9
Mike Krisher <mike@mikekrisher.com>
2021-09-23 09:56:47 -0400

    Adds default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "schemabook_production"
 
   config.action_mailer.perform_caching = false
-  #config.action_mailer.default_url_options = { host: 'schemabook-cm6f.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'schemabook-cm6f.onrender.com' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV['MAILGUN_API_KEY'],
