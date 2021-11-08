a2859e1662f89a4bdfd9ad7290cbdf18530d0d8d
Mike Krisher <mike@mikekrisher.com>
2021-11-08 08:37:27 -0500

    Adds services to schema model

--- app/models/schema.rb
+++ app/models/schema.rb
@@ -2,8 +2,8 @@ class Schema < ApplicationRecord
   FORMATS = ["JSON Schema", "Avro", "CSV"]
 
   belongs_to :team
+  belongs_to :service, optional: :true
 
-  validates :name, presence: true
-  validates :name, uniqueness: true # TODO: scope to business or area { scope: :business }
+  validates :name, presence: true, uniqueness: { scope: :service_id }
   validates :team_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_202522) do
+ActiveRecord::Schema.define(version: 2021_11_08_130935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 2021_10_23_202522) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "team_id"
+    t.bigint "service_id"
+    t.index ["name", "service_id"], name: "index_schemas_on_name_and_service_id", unique: true
+    t.index ["service_id"], name: "index_schemas_on_service_id"
     t.index ["team_id"], name: "index_schemas_on_team_id"
   end
 
@@ -82,4 +85,5 @@ ActiveRecord::Schema.define(version: 2021_10_23_202522) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["team_id"], name: "index_users_on_team_id"
   end
+
 end
