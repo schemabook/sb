3e98b9d1ce86f8a1403315e59cfb1d578dcb74f6
Mike Krisher <mike@mikekrisher.com>
2021-11-09 08:08:49 -0500

    validates schema name length

--- app/models/schema.rb
+++ app/models/schema.rb
@@ -3,7 +3,7 @@ class Schema < ApplicationRecord
   belongs_to :service, optional: true
   belongs_to :format
 
-  validates :name, presence: true, uniqueness: { scope: :service_id }
+  validates :name, presence: true, uniqueness: { scope: :service_id }, length: { in: 2..120 }
   validates :team_id, presence: true
   validates :format_id, presence: true
 end
