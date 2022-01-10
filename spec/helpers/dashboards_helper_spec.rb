9e4d9711b6aec61e6dd1195387aa07c86ebe9748
Mike Krisher <mike@mikekrisher.com>
2022-01-10 15:27:52 -0500

    Abstracts right sidebar into partial and helpers

---
+++ spec/helpers/dashboards_helper_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe DashboardsHelper, :type => :helper do
+  describe "#activity_title" do
+    let(:resource) { create(:business, name: "foo") }
+    let(:activity) { create(:activity, :with_activity_log, user: create(:user), resource_class: resource.class.to_s, resource_id: resource.id, title: "Created", detail: "bar") }
+
+    it "returns the title including link to resource" do
+      expect(helper.activity_title(activity)).to eq("Created <a class=\"text-sm text-cyan-600\" href=\"/businesses/#{resource.id}\">#{resource.name}</a>")
+    end
+  end
+end
