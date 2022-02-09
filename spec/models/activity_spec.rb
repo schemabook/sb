447e78e95941a8b3bbffc33f505ea8dee9d4ab14
Mike Krisher <mike@mikekrisher.com>
2022-02-08 20:14:06 -0500

    Adds Activity#for_user scope

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -12,6 +12,25 @@ RSpec.describe Activity, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :detail }
 
+  describe "self.for_user" do
+    context "with activities for users" do
+      let(:activity_1) { create(:activity, :with_activity_log, user_id: user.id) }
+      let(:activity_2) { create(:activity, :with_activity_log, user_id: user.id) }
+
+      it "returns the activities" do
+        expect(described_class.for_user(user.id)).to match_array([activity_1, activity_2])
+      end
+    end
+
+    context "without activities for users" do
+      let(:user) { create(:user) }
+
+      it "returns an empty array" do
+        expect(described_class.for_user(user.id)).to match_array([])
+      end
+    end
+  end
+
   describe "#resource" do
     let!(:resource) { create(:business) }
     let!(:activity) { create(:activity, :with_activity_log, user: user, title: "foo", detail: "bar", resource_class: resource.class.to_s, resource_id: resource.id) }
