d78b6a72ba4eddb519de9de36ac125c7fac596d0
Mike Krisher <mike@mikekrisher.com>
2022-02-08 20:28:10 -0500

    Adds ActivityLog#for_user

--- spec/models/activity_log_spec.rb
+++ spec/models/activity_log_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe ActivityLog, type: :model do
   it { should belong_to :business }
   it { should have_many :activities }
   it { should validate_presence_of :business_id }
+
+  describe "for_user" do
+    let!(:user)      { create(:user) }
+    let!(:user2)     { create(:user, email: "foo@example.com") }
+    let!(:activity)  { create(:activity, activity_log: subject, user: user) }
+    let!(:activity2) { create(:activity, activity_log: subject, user: user2) }
+
+    it "should return all activities for a given user" do
+      expect(subject.for_user(user_id: user.id)).to match_array([activity])
+    end
+  end
 end
