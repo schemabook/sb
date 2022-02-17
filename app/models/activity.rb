4c617da4d7d35ed43f4031df1f95dcedf394b0ea
Mike Krisher <mike@mikekrisher.com>
2022-02-16 20:55:04 -0500

    Adds ActivityLog#for_service

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,6 +8,7 @@ class Activity < ApplicationRecord
   validates :detail, presence: true
 
   scope :for_user, ->(user_id) { where(user_id: user_id) }
+  scope :for_service, ->(service_id) { where(resource_class: 'Service', resource_id: service_id) }
 
   def resource
     resource_class.constantize.find(resource_id)
