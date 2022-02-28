c0686346b830c6f4ef82bc51a7a6a5215396971d
Mike Krisher <mike@mikekrisher.com>
2022-02-28 10:00:10 -0500

    Updates schema factory to include :with_format_and_body trait

--- spec/factories/schema.rb
+++ spec/factories/schema.rb
@@ -7,5 +7,11 @@ FactoryBot.define do
     trait :with_team do
       team { create(:team, :with_business) }
     end
+
+    trait :with_format_and_body do
+      file_type { "json" }
+      format    { create(:format, file_type: :json) }
+      body      { '{"foo": {"bar": 1}}' }
+    end
   end
 end
