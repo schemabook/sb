362febda303815713cfd12249d6e7c1a2841e6c5
Mike Krisher <mike@mikekrisher.com>
2022-03-12 13:26:59 -0500

    Adds subscriber for updated.business to write activities

---
+++ app/subscribers/subscribers/businesses/updated/activity_log.rb
@@ -0,0 +1,33 @@
+module Subscribers
+  module Businesses
+    module Updated
+      class ActivityLog
+        include Subscribers::Subscriber
+
+        EVENT_NAME = Events::Businesses::Updated::EVENT_NAME
+
+        def initialize
+          subscribe
+        end
+
+        # rubocop:disable Metrics/MethodLength
+        def process(event:)
+          payload  = JSON.parse(event.payload.to_json, object_class: OpenStruct)
+          business = Business.find(payload.after.id)
+          user     = User.find(payload.after.actor_id)
+          log      = ::ActivityLog.create(business: business)
+
+          Activity.create(
+            activity_log: log,
+            user: user,
+            title: "Updated Business",
+            detail: "Updated account settings for #{business.name}",
+            resource_id: business.id,
+            resource_class: business.class.to_s
+          )
+        end
+        # rubocop:enable Metrics/MethodLength
+      end
+    end
+  end
+end
---
+++ spec/subscribers/businesses/updated/activity_log_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe Subscribers::Businesses::Updated::ActivityLog do
+  subject { described_class.new }
+
+  it "defines an event name" do
+    expect(subject.class::EVENT_NAME).to eq(Events::Businesses::Updated::EVENT_NAME)
+  end
+
+  it "subscribes when instantiated" do
+    expect_any_instance_of(described_class).to receive(:subscribe)
+
+    described_class.new
+  end
+
+  describe "#process" do
+    let(:business) { create(:business) }
+    let(:user)     { create(:user) }
+
+    it "persists an ActivityLog object" do
+      payload    = Events::Businesses::Updated.new(business: business, user: user).payload
+      event_type = "foo"
+      event      = ActiveSupport::Notifications::Event.new(
+        event_type,
+        Time.zone.now - 1.second,
+        Time.zone.now,
+        123,
+        payload
+      )
+
+      expect {
+        subject.process(event: event)
+      }.to change(Activity, :count)
+    end
+  end
+end
+
