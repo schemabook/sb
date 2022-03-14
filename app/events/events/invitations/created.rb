7e34656a70c54ed8414357d19e0361e37e4118f9
Mike Krisher <mike@mikekrisher.com>
2022-03-14 08:36:02 -0400

    [SCH-95] Introduces Invitations created event

---
+++ app/events/events/invitations/created.rb
@@ -0,0 +1,65 @@
+module Events
+  module Invitations
+    class Created
+      include Events::Event
+
+      EVENT_NAME    = "created.invitiation".freeze
+      EVENT_VERSION = "0.1".freeze
+
+      attr_accessor :teammate
+
+      def initialize(teammate:, user:)
+        @teammate = teammate # is a User
+        @user     = user
+      end
+
+      # rubocop:disable Metrics/MethodLength
+      def schema
+        Avro::Builder.build do
+          namespace EVENT_NAME
+
+          record :teammate do
+            optional :before, :record
+
+            required :after, :record do
+              required :id, :int
+              required :actor_id, :int
+            end
+
+            required :source, :record do
+              required :version, :string
+              required :name, :string
+              required :ts_ms, :long, logical_type: 'timestamp-millis'
+              required :table, :string
+              optional :query, :string
+            end
+
+            required :op, :enum, symbols: [:c, :u, :d]
+            required :ts_ms, :long, logical_type: 'timestamp-millis'
+          end
+        end
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      # rubocop:disable Metrics/MethodLength
+      def payload
+        {
+          before: nil,
+          after: {
+            id: @teammate.id,
+            actor_id: @user.id
+          },
+          source: {
+            version: EVENT_VERSION,
+            name: EVENT_NAME,
+            ts_ms: (@teammate.created_at.to_f * 1000).to_i,
+            table: @teammate.class.table_name
+          },
+          op: 'c',
+          ts_ms: (Time.zone.now.to_f * 1000).to_i
+        }
+      end
+      # rubocop:enable Metrics/MethodLength
+    end
+  end
+end
