84035d812954438061b845fb788774cd3cd79c15
Mike Krisher <mike@mikekrisher.com>
2022-06-20 14:37:04 -0400

    [adhoc] small tweaks

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -2,7 +2,7 @@ module DashboardsHelper
   def activity_title(activity)
     resource      = activity.resource
     path          = resource.is_a?(User) ? user_profile_path(resource) : polymorphic_path(resource)
-    resource_link = link_to(resource.name, path, { class: 'text-sm text-cyan-600' })
+    resource_link = link_to(truncate(resource.name, length: 30), path, { class: 'text-sm text-cyan-600' })
 
     "#{activity.title} #{resource_link}".html_safe
   end
