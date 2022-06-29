bdba0ed2fe92dc6c13a26794f3d29813b4b7e332
Mike Krisher <mike@mikekrisher.com>
2022-06-28 20:17:49 -0400

    Assigns schemas in dashboard controller

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe DashboardsController, type: :controller do
   let!(:business) { create(:business, :with_activity_log) }
   let!(:user)     { create(:user, business: business) }
   let!(:activity) { create(:activity, activity_log: business.activity_log, user: user) }
+  let!(:team)     { create(:team, business: business) }
+  let!(:schema)   { create(:schema, :with_format_and_body, team: team) }
 
   before do
     sign_in user
@@ -20,6 +22,12 @@ RSpec.describe DashboardsController, type: :controller do
       get :index
 
       expect(assigns(:activities)).to eq([activity])
+    end
+
+    it "assigns @schemas" do
+      get :index
+
+      expect(assigns(:schemas)).to eq([schema])
     end
   end
 end
