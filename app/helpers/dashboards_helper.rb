83fc6bb4b14e7a8ffb802248f94cae0db3e430f6
Mike Krisher <mike@mikekrisher.com>
2022-07-25 07:58:09 -0400

    Adds avatar helper method and makes stakeholders on the dashboard dynamic

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -6,4 +6,31 @@ module DashboardsHelper
 
     "#{activity.title} #{resource_link}".html_safe
   end
+
+  def small_avatar(user, add_styles=nil)
+    size = 5
+    styles = "inline-block h-#{size} w-#{size} rounded-full ring-2 ring-white "
+    styles += add_styles
+
+    if user.avatar.attached?
+      image_tag user.avatar.variant(resize: "#{size}x#{size}!"), class: styles
+    else
+      # NOTE: we default to gravatar which will return a default image if the user doesn't have an account
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+      image_tag gravatar_url, alt: user.display_name, class: styles
+    end
+  end
+
+  def stakeholders(stakeholders)
+    results = ""
+    stakeholders.each_with_index do |stakeholder, index|
+      add_styles = ""
+      add_styles += "-ml-1" if index > 0
+
+      results += small_avatar(stakeholder.user, add_styles)
+    end
+
+    results.html_safe
+  end
 end
