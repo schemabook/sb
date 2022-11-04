970d9d42fdba2df2d044da1e74cb131b7a365edb
Mike Krisher <mike@mikekrisher.com>
2022-11-04 14:35:24 -0400

    Adds subscriber to create an activity when a version is created

---
+++ app/subscribers/subscribers/versions/created/activity.rb
@@ -0,0 +1,37 @@
+module Subscribers
+  module Versions
+    module Created
+      class Activity
+        include Subscribers::Subscriber
+
+        EVENT_NAME = Events::Versions::Created::EVENT_NAME
+
+        def initialize
+          subscribe
+        end
+
+        def process(event:)
+          payload = JSON.parse(event.payload.to_json, object_class: OpenStruct)
+          version = Version.where(id: payload.after.id).first
+          user    = User.find(payload.after.actor)
+          log     = user.business.activity_log
+
+          create_activity(log:, version:, user:)
+        end
+
+        private
+
+        def create_activity(log:, version:, user:)
+          ::Activity.create(
+            activity_log: log,
+            user:,
+            title: "Created Version",
+            detail: "Created version #{version.index} for #{version.schema.name}",
+            resource_id: version.id,
+            resource_class: version.class.to_s
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/subscribers/versions/created/activity_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe Subscribers::Versions::Created::Activity do
+  subject { described_class.new }
+
+  it "defines an event name" do
+    expect(subject.class::EVENT_NAME).to eq(Events::Versions::Created::EVENT_NAME)
+  end
+
+  it "subscribes when instantiated" do
+    expect_any_instance_of(described_class).to receive(:subscribe)
+
+    described_class.new
+  end
+
+  describe "#process" do
+    let(:format) { create(:format, file_type: :json) }
+    let(:user)   { create(:user) }
+    let(:schema) { create(:schema, :with_team, name: "foo", format:) }
+    let(:version) { create(:version, schema: schema) }
+
+    it "persists an Activity object" do
+      payload    = Events::Versions::Created.new(record: version, user:).payload
+      event_type = "foo"
+      event      = ActiveSupport::Notifications::Event.new(
+        event_type,
+        1.second.ago,
+        Time.zone.now,
+        123,
+        payload
+      )
+
+      expect {
+        subject.process(event:)
+      }.to change(Activity, :count)
+    end
+  end
+end
+
