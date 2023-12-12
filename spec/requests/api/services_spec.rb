8eecc5e6e4019de92ff5292d2858f4226d2bbd57
Mike Krisher <mike@mikekrisher.com>
2023-12-12 13:35:00 -0500

    Adds response examples

--- spec/requests/api/services_spec.rb
+++ spec/requests/api/services_spec.rb
@@ -39,6 +39,18 @@ RSpec.describe 'api/services', type: :request do
         }
       }
     end
+
+    content = example.metadata[:response][:content] || {}
+    example_spec = {
+      "application/json"=>{
+        examples: {
+          test_example: {
+            value: JSON.parse(response.body, symbolize_names: true)
+          }
+        }
+      }
+    }
+    example.metadata[:response][:content] = content.deep_merge(example_spec)
   end
 end
 # rubocop:enable RSpec/VariableName
