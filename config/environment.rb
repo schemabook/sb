f881b144847d0307795480069120904fc3a79f0d
Mike Krisher <mike@mikekrisher.com>
2023-12-18 11:20:51 -0500

    lints

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,9 @@ require_relative "application"
 
 # use BetterStack logger
 unless Rails.env.test?
-   http_device = Logtail::LogDevices::HTTP.new(ENV.fetch("BETTERSTACK_TOKEN"))
-   Rails.logger = Logtail::Logger.new(http_device)
+  http_device = Logtail::LogDevices::HTTP.new(ENV.fetch("BETTERSTACK_TOKEN"))
+  Rails.logger = Logtail::Logger.new(http_device)
 end
-
 
 # Initialize the Rails application.
 Rails.application.initialize!
