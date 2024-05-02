3e6bc5f511c57329917f710df15ba7f7e12305c1
Mike Krisher <mike@mikekrisher.com>
2024-05-02 11:36:31 -0400

    Sorts the language around subscription and cancellation using Stripe as the source

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -6,6 +6,12 @@ class BusinessesController < ApplicationController
     @stakeholders = User.where(business_id: @business.id)
     @teams = Team.where(business_id: @business.id)
     @activities = @business.activity_log.for_business(business: @business).limit(8)
+
+    subscription = Stripe::Subscription.retrieve @business.subscription_id 
+
+    @cancelled = subscription[:ended_at].nil? ? nil : Time.at(subscription[:ended_at]).to_date
+    @days_until_due = subscription[:days_until_due]
+    @next_bill_date = Date.today + @days_until_due.to_i.days
   end
 
   def edit
